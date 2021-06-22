27db0664ed1c367fcb679bf6f74a2d6ab01142d0
sepehr jafari <sepehrj1415@gmail.com>
2021-06-22 08:56:18 +0430

    create the instance of client in Main.java

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,5 +1,6 @@
 package Model;
 
+import Model.Client.Client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    public static Client client;
     @Override
     public void start(Stage primaryStage) throws Exception{
         PageLoader.initStage(primaryStage);
@@ -16,6 +17,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        client = new Client();
         launch(args);
     }
 }
