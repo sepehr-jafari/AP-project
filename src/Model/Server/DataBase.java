3ec8f1dd4ff40758b546b0ec2ece71e1f8ad5a46
sepehr jafari <sepehrj1415@gmail.com>
2021-06-22 09:04:57 +0430

    change add client method for saving clients in file

--- src/Model/Server/DataBase.java
+++ src/Model/Server/DataBase.java
@@ -2,42 +2,73 @@ package Model.Server;
 
 import Model.Person;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 
 public class DataBase {
-    private boolean firstTime = true;
     private static DataBase dataBase;
-    private static File file = new File("F:\\AP-project\\src\\Model\\Server\\Data\\clientsAccount.txt");
-    private static ConcurrentHashMap<Person, String> clientsAccount = new ConcurrentHashMap<>();
-    private DataBase(){}
+    private static File file;
+    private final static String fileAddress ="F:\\AP-project\\src\\Model\\Server\\Data\\clientsAccount.txt";
+
+    private ConcurrentHashMap<Person, String> clientsAccount;
+    private final Semaphore LockForAddingClient_FinalList = new Semaphore(1);
+    private final Semaphore LockForInitializeDatabase = new Semaphore(1);
+    private final Semaphore LockForAddingClient_ClientHandler = new Semaphore(1);
+
+    private DataBase() {
+        clientsAccount = new ConcurrentHashMap<>();
+    }
+
     public static DataBase getDataBase() {
-        if (dataBase==null){
-            if (file.exists()){
+        if (dataBase == null) {
+            dataBase = new DataBase();
+            file = new File(fileAddress);
+            if (!file.exists()){
                 try {
-                    FileInputStream fis = new FileInputStream(file);
-                    ObjectInputStream ois = new ObjectInputStream(fis);
-                    clientsAccount = (ConcurrentHashMap<Person, String>) ois.readObject();
-                    ois.close();
-                    fis.close();
-                }catch (IOException e){
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
+                    file.createNewFile();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }else {
-                dataBase = new DataBase();
             }
         }
         return dataBase;
     }
-    public ConcurrentHashMap<Person, String> getData(){
+
+    public ConcurrentHashMap<Person, String> getData() {
+        if (clientsAccount.isEmpty()&&file.length()!=0) {
+            try {
+                LockForInitializeDatabase.acquire();
+                FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                this.clientsAccount = (ConcurrentHashMap<Person, String>) ois.readObject();
+                ois.close();
+                fis.close();
+                LockForInitializeDatabase.release();
+            } catch (IOException | ClassNotFoundException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
         return clientsAccount;
     }
-    public void addClient(Person person){
-        clientsAccount.put(person, person.getPassword());
+
+    public void AddClient_InClientHandler(Person person) {
+        try {
+            LockForAddingClient_ClientHandler.acquire();
+            clientsAccount.put(person, person.getPassword());
+            FileOutputStream os = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(clientsAccount);
+            oos.close();
+            os.close();
+            LockForAddingClient_ClientHandler.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
