dbbfa836c6e518d8ef959ab2b3883c3cf89dc678
sepehr jafari <sepehrj1415@gmail.com>
2021-06-22 09:06:27 +0430

    remove update database method in server

--- src/Model/Server/Server.java
+++ src/Model/Server/Server.java
@@ -3,20 +3,28 @@ package Model.Server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Server {
+    public final static int PORT = 2222;
+    private static boolean isServerUp = true;
+    private final DataBase Data = DataBase.getDataBase();
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(5056);
-        Socket client;
-        while (true) {
-            client = null;
+        ServerSocket server = new ServerSocket(PORT);
+
+
+        while (isServerUp) {
+            Socket client= null;
             try {
                 client = server.accept();
+
                 OutputStream os = client.getOutputStream();
                 ObjectOutputStream oos = new ObjectOutputStream(os);
                 InputStream is = client.getInputStream();
                 ObjectInputStream ois = new ObjectInputStream(is);
-                Thread thread = new Thread(new ClientHandler(client, ois, oos));
+                ConnectionHandler CH = new ConnectionHandler(oos,ois);
+                Thread thread = new Thread(new ClientHandler(CH));
                 thread.start();
             } catch (IOException e) {
                 e.printStackTrace();
