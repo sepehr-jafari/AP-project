9c7e740b7d5db800bb9d184526e6a652be3d8060
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:11:28 +0430

    create a constructor and some system.out in server side

--- src/Model/Message/ConnectMessage2.java
+++ src/Model/Message/ConnectMessage2.java
@@ -2,6 +2,9 @@ package Model.Message;
 
 import Model.Server.ConnectionHandler;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ConnectMessage2 extends Message2{
     public static final long serialVersionUID = 12345678L;
     public final String username;
@@ -11,6 +14,11 @@ public class ConnectMessage2 extends Message2{
     }
     @Override
     public void handle(ConnectionHandler connectionHandler) {
+        System.out.println("action: connect");
+        System.out.println(username + " connect");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
         connectionHandler.SendMessage(new TextMessage2("The user"+this.username+"connected."));
     }
 }
