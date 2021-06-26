06d9f39395b7e6512d3b0089e75e11da2c435028
sepehr jafari <sepehrj1415@gmail.com>
2021-06-26 23:59:08 +0430

    by GetNumberOfRepostMessage client side get the number of repost of specified post

---
+++ src/Model/Message/GetNumberOfRepostMessage.java
@@ -0,0 +1,18 @@
+package Model.Message;
+
+import Model.Server.ConnectionHandler;
+
+public class GetNumberOfRepostMessage extends Message2{
+    public static final long serialVersionUID = 287263109L;
+    private String UID;
+
+    public GetNumberOfRepostMessage(String UID) {
+        this.UID = UID;
+    }
+
+    @Override
+    public void handle(ConnectionHandler connectionHandler) {
+        Long numberOfRepost = connectionHandler.getDataBase().getNumberOfRepost(UID);
+        connectionHandler.SendMessage(new ObjectMessage(numberOfRepost));
+    }
+}
