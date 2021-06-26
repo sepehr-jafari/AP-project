07f80df0d85ac4b0708cc68737de88a882523a02
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:32:04 +0430

    add an other initialization to server

--- src/Model/Server/Server.java
+++ src/Model/Server/Server.java
@@ -9,7 +9,12 @@ import java.util.TimerTask;
 public class Server {
     public final static int PORT = 2222;
     private static boolean isServerUp = true;
-    private final DataBase Data = DataBase.getDataBase();
+    private static final DataBase Data = DataBase.getDataBase();
+    static {
+        Data.InitializeClientsAccount();
+        Data.InitializeListOfUsers();
+        Data.InitializeLikesAndReposts();
+    }
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(PORT);
 
@@ -24,7 +29,7 @@ public class Server {
                 InputStream is = client.getInputStream();
                 ObjectInputStream ois = new ObjectInputStream(is);
                 ConnectionHandler CH = new ConnectionHandler(oos,ois);
-                Thread thread = new Thread(new ClientHandler(CH));
+                Thread thread = new Thread(new ClientHandler(CH, client));
                 thread.start();
             } catch (IOException e) {
                 e.printStackTrace();
