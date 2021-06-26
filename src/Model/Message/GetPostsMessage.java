9aaf281d6cfd04a97640118ec65b0b139476cd1b
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 00:01:21 +0430

    by GetPostsMessage client side get all posts that should show in TimeLine page and other page from server

---
+++ src/Model/Message/GetPostsMessage.java
@@ -0,0 +1,42 @@
+package Model.Message;
+
+import Model.Person;
+import Model.Post;
+import Model.Server.ConnectionHandler;
+import javafx.geometry.Pos;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class GetPostsMessage extends Message2{
+    public static final long serialVersionUID = 25762586523L;
+    private final Person person;
+
+    public GetPostsMessage(Person person) {
+        this.person = person;
+    }
+    @Override
+    public void handle(ConnectionHandler connectionHandler) {
+        String username = person.getUsername();
+        ArrayList<String> followers = person.getFollowing();
+        ConcurrentLinkedQueue<Person> users = connectionHandler.getDataBase().getListOfUsers();
+        ArrayList<ArrayList<Post>> posts = new ArrayList<>();
+        for (Person p:users) {
+            for (int i = 0; i < followers.size(); i++) {
+                if (followers.get(i).equals(p.getUsername())){
+                    posts.add(p.getPosts());
+                    break;
+                }
+            }
+        }
+        posts.add(person.getPosts());
+        System.out.println("action: get posts");
+        System.out.println(person.getUsername() + " get post list");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
+        connectionHandler.SendMessage(new ObjectMessage(posts));
+    }
+}
