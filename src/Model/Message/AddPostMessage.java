521c5df7c1d57fb9e27c60af49de4b1ca0463f7b
sepehr jafari <sepehrj1415@gmail.com>
2021-06-26 23:46:44 +0430

    by AddPostMessage one post add to user profile in server side

---
+++ src/Model/Message/AddPostMessage.java
@@ -0,0 +1,31 @@
+package Model.Message;
+
+import Model.Post;
+import Model.Server.ConnectionHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class AddPostMessage extends Message2{
+    public static final long serialVersionUID = 2457541254L;
+    private final Post post;
+
+    public AddPostMessage(Post post) {
+        this.post = post;
+    }
+
+    @Override
+    public void handle(ConnectionHandler connectionHandler) {
+        connectionHandler.getDataBase().AddPost_InClientHandler(post);
+        connectionHandler.getDataBase().updateLikesAndReposts();
+        System.out.println("action: publish post");
+        System.out.println(post.getUsername() + " publish");
+        System.out.println("message: " + post.getTitle() + " " + post.getUsername());
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
+
+        connectionHandler.SendMessage(new TextMessage2("New post added"));
+    }
+}
