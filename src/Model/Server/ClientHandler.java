8df29a47f51b8ed59af781baa8733fe4e60d485f
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:24:35 +0430

    handling client exit in ClientHandler class

--- src/Model/Server/ClientHandler.java
+++ src/Model/Server/ClientHandler.java
@@ -3,12 +3,15 @@ package Model.Server;
 import Model.Message.Message2;
 
 import java.io.*;
+import java.net.Socket;
 
 public class ClientHandler implements Runnable{
     volatile private ConnectionHandler ch;
+    volatile private Socket socket;
 
-    public ClientHandler(ConnectionHandler ch) {
+    public ClientHandler(ConnectionHandler ch, Socket socket) {
         this.ch = ch;
+        this.socket = socket;
     }
 
 
@@ -20,7 +23,15 @@ public class ClientHandler implements Runnable{
                 Message2 message = (Message2) ch.getInputStream().readObject();
                 message.handle(ch);
             } catch (IOException e) {
-                e.printStackTrace();
+
+                ch.close();
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                break;
+
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
