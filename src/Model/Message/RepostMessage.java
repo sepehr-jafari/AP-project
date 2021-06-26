02da9941ec56d5d843223802b536242dc34d9450
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 00:10:10 +0430

    by RepostMessage one post add to user posts in server

---
+++ src/Model/Message/RepostMessage.java
@@ -0,0 +1,23 @@
+package Model.Message;
+
+import Model.Post;
+import Model.Server.ConnectionHandler;
+import javafx.geometry.Pos;
+
+public class RepostMessage extends Message2{
+    public static final long serialVersionUID =67500077L;
+    private Post post;
+    private String username;
+
+    public RepostMessage(Post post, String username) {
+        this.post = post;
+        this.username = username;
+    }
+
+    @Override
+    public void handle(ConnectionHandler connectionHandler) {
+        connectionHandler.getDataBase().AddThisPostToMyPosts(post,username);
+        connectionHandler.getDataBase().updateListOfUser();
+        connectionHandler.SendMessage(new TextMessage2("The post add to your post"));
+    }
+}
