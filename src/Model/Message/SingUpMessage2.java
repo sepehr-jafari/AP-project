f6dc8fd3effef24c0a8dd7b8f59a39e76a20a2ad
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:16:46 +0430

    set Appropriate system.out for server side in singUpMessage2

--- src/Model/Message/SingUpMessage2.java
+++ src/Model/Message/SingUpMessage2.java
@@ -2,6 +2,10 @@ package Model.Message;
 
 import Model.Person;
 import Model.Server.ConnectionHandler;
+import Model.Server.DataBase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SingUpMessage2 extends Message2{
     public static final long serialVersionUID = 16519891324L;
@@ -14,7 +18,14 @@ public class SingUpMessage2 extends Message2{
     }
     @Override
     public void handle(ConnectionHandler connectionHandler) {
-        connectionHandler.getDataBase().AddClient_InClientHandler(person);
+        DataBase dataBase =connectionHandler.getDataBase();
+        dataBase.AddClient_InClientHandler(person);
+        dataBase.SaveUser(person);
+        System.out.println("action: sign up");
+        System.out.println(person.getUsername() + " register " + "account");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
         connectionHandler.SendMessage(new TextMessage2("The Account was created."));
 
     }
