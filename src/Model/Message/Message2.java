0973c279183e464d26c5488f9dc7e17cc2a893a5
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:14:05 +0430

    create some getter and setter fot Message2 super class

--- src/Model/Message/Message2.java
+++ src/Model/Message/Message2.java
@@ -6,11 +6,20 @@ import java.io.Serializable;
 
 public abstract class Message2 implements Serializable {
     protected String p;
+    protected Object o;
     public abstract void handle(ConnectionHandler connectionHandler);
     public void setP(String P){
         this.p=P;
     }
     public String getP(){
         return p;
+    }
+
+    public void setO(Object o) {
+        this.o = o;
+    }
+
+    public Object getO() {
+        return o;
     }
 }
