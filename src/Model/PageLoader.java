40a2db771eda60689244cd56c4f7d54856983fdf
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 01:19:31 +0430

    create an other load method

--- src/Model/PageLoader.java
+++ src/Model/PageLoader.java
@@ -1,5 +1,4 @@
 package Model;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,6 +8,7 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+
 
 public class PageLoader {
     private static Stage stage;
@@ -23,9 +23,17 @@ public class PageLoader {
         stage.setTitle("SBU GRAM");
         stage.setWidth(WIDTH);
         stage.setHeight(HEIGHT);
-        stage.initStyle(StageStyle.DECORATED);
-        stage.setResizable(true);
+        stage.initStyle(StageStyle.DECORATED); //the three buttons on top of stage will be shown
+        stage.setResizable(false); //the resizable button is enabled. if your design is responsive this must be true
         stage.getIcons().add(new Image(Paths.get("F:/images/87390.jpg").toUri().toString()));
+    }
+
+    public void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    public static Stage getPrimaryStage() {
+        return stage;
     }
 
     public Parent loadFXML(String fxml) throws IOException {
@@ -33,11 +41,18 @@ public class PageLoader {
         return fxmlLoader.load();
     }
 
-
+    //load fxmls with already set controllers
     public void load(String url) throws IOException {
         scene = new Scene(new PageLoader().loadFXML(url));
         stage.setScene(scene);
         stage.show();
     }
 
+    //load fxmls with a special controller set in the runtime
+    public void load(String fxml, Object controller) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/View/" + fxml + ".fxml"));
+        fxmlLoader.setController(controller);
+        fxmlLoader.setLocation(getClass().getResource("/View/" + fxml + ".fxml"));
+        fxmlLoader.load();
+    }
 }
