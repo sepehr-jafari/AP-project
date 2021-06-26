16dca6ef04d3de7e9d4a573c48f0e1be3203fbbf
sepehr jafari <sepehrj1415@gmail.com>
2021-06-26 23:13:08 +0430

    create CommentsController class to initialize comments item

---
+++ src/Controller/CommentsController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import Model.Comment;
+import Model.Main;
+import Model.Message.AddCommentMessage;
+import Model.PageLoader;
+import Model.Post;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CommentsController {
+    private static ArrayList<Comment> commentArrayList;
+    public ListView<Comment> comments;
+    public TextArea comment;
+    private static Post post;
+
+    @FXML
+    public void initialize(){
+        if (!commentArrayList.isEmpty()) {
+            comments.setItems(FXCollections.observableArrayList(commentArrayList));
+            comments.setCellFactory(comments -> new CommentItem());
+        }
+    }
+    public static void setComments(ArrayList<Comment> comments){
+        commentArrayList = comments;
+    }
+
+    public void Exit(ActionEvent actionEvent) {
+        try {
+            new PageLoader().load("TimeLine");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendComment(ActionEvent actionEvent) {
+        String commentToSend = comment.getText();
+        String commenterUsername = LoginController.getUsername();
+        Comment comment = new Comment(commenterUsername,commentToSend,ProfileController.profile.getImage());
+        Main.client.getResponse(new AddCommentMessage(post.getUID(), comment, post.getUsername(), post));
+    }
+
+    public static void setPost(Post Post) {
+        post = Post;
+    }
+}
