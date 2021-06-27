eb6b0c6eccffb47b3619d63a45a85269c6f19d11
sepehr jafari <sepehrj1415@gmail.com>
2021-06-27 08:48:12 +0430

    create module for javafx

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module AP.project {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
     opens View;
     opens Model;
     opens Controller;
